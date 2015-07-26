a40f2a71a178ad95be148f55ec569cf8e9f7b897
Arif <arif@nomis.com>
2015-07-26 02:24:28 -0700

    Added yes and no feature on homepage.

--- client/app/controllers/events.controller.js
+++ client/app/controllers/events.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('synerApp')
-  .controller('EventCtrl', function ($scope, EventService) {
+  .controller('EventCtrl', function ($scope, EventService, AuthService) {
 
     //Local variables
     $scope.allEvents = [];
@@ -27,11 +27,49 @@ angular.module('synerApp')
     $scope.getEvent = function(id) {
 
       EventService.getEvent(id).success(function(data){
-        console.log("Event added.");
+        console.log("Event retrieved.");
       });
       
     };
 
+    $scope.willAttend = function(eventid) {
+
+        //Get current user logged in
+        var user = AuthService.currentUser();
+        var data = 
+          {
+              id        : eventid,
+              attendee  : user
+          }
+
+        //Sent user information to be added to Event document
+        EventService.willAttend(data).success(function(data, status, headers) {
+              $scope.isAttending = true;
+              $scope.attendingStatus = "Not Attending"; //Status reversed to show proper option on UI
+              $scope.init(); //Fill table with recent data from DB.
+              console.log("Document updated.");
+
+          });
+    }
+
+    $scope.willNotAttend = function(eventid) {
+        //Get current user logged in
+        var user = AuthService.currentUser();
+        var data = 
+          {
+              id        : eventid,
+              attendee  : user
+          }
+
+        //Sent user information to be added to Event document
+        EventService.willNotAttend(data).success(function(data, status, headers) {
+              $scope.isAttending = false;
+              $scope.attendingStatus = "Attend"; //Status reversed to show proper option on UI
+              $scope.init(); //Fill table with recent data from DB.
+              console.log("Document updated.");
+
+          });
+    }
 
     //Delete an Event /////////////////////////////////////////////////////////////////////
     $scope.deleteEvent = function(event) {
@@ -43,5 +81,4 @@ angular.module('synerApp')
        // $scope.eventName = "";
 
     }
-
   });
